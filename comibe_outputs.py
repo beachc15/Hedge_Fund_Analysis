e6b4a00d8c021e980c841ff4de62cb783d02f14e
beachc15 <60740158+beachc15@users.noreply.github.com>
2020-05-03 09:24:40 -0700

    final combination file finished. added functionality for unique tickers

--- comibe_outputs.py
+++ comibe_outputs.py
@@ -2,16 +2,44 @@ import json
 from os import listdir
 from os.path import isfile, join
 
+import pandas as pd
+
 uri = 'outputs/'
 
 onlyfiles = [f for f in listdir(uri) if isfile(join(uri, f))]
 i = 0
-x = {}
+dfs = {}
+tickers = []
+nan_names = []
 for file in onlyfiles:
-	print(i)
 	i += 1
 	with open(f'outputs/{file}', 'r') as inp:
-		x[i] = inp.read()
+		x = (json.load(inp))
+		for y in x:
+			z = json.loads(x[y])
+			for p in z:
+				dfs[y] = (pd.DataFrame(z))
+			# find the errors in the ticker value and add them to a list to fix later
+			nans = dfs[y][(dfs[y]['ticker'].isna()) == True].index
+			unique = dfs[y][(dfs[y]['ticker'].isna()) != True].index
+			dfs[y].drop(nans)
+			for ind in nans:
+				nan_names.append(ind)
+			for unq in unique:
+				tickers.append(unq)
+			dfs[y] = dfs[y].to_json()
 
-with open('outputs/export.json', 'w') as out:
-	json.dump(x, out)
+# save error stats into a stats file to keep track of errors
+nan_names = pd.Series(nan_names).drop_duplicates()
+ticker_lookup_error_count = len(nan_names)
+nan_names = nan_names.to_json()
+
+with open('inputs/stats.txt', 'w') as out:
+	out.write(f'ticker Lookup Error Count: {ticker_lookup_error_count}')
+	out.write(nan_names)
+
+with open('inputs/unique.json', 'w') as out:
+	json.dump(tickers, out)
+
+with open('inputs/export.json', 'w') as out:
+	json.dump(dfs, out)
